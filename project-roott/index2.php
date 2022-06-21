fc717d66ef0276741990197f277ae9e6f37b0e87
Joran Schrievers <99062987@mydavinci.nl>
2022-06-21 12:57:37 +0200

    added the codeigniter files

---
+++ project-roott/index2.php
@@ -0,0 +1,20 @@
+<?php
+//require "app/Config/Database.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign in screen</title>
+</head>
+<body>
+    <form action="post">
+        <input type="text" placeholder="username" id="usernameInput">
+        <input type="password" name="password" id="passwordInput">
+        <input type="email" name="email">
+    </form>
+</body>
+</html>
